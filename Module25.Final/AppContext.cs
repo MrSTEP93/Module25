c7dfcccbe676a2d67f842a9fcc1cd77f53293712
Shatalov.AG <mrstep93@ya.ru>
2024-06-30 00:09:21 +0300

    add final proj

---
+++ Module25.Final/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module25.Final
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Book> Book { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = File.ReadAllText("connString.txt");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ Module25.Final/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Module25.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to digital library!");
+            Console.WriteLine("Connecting to db...");
+            try
+            {
+                using (var db = new AppContext())
+                {
+                    Console.WriteLine("Connected successfuly!");
+                    var user1 = new Client { Name = "Arthur", Email = "archi@ya.ru" };
+                    var user2 = new Client { Name = "Klim", Email = "KilmSanych@ya.ru" };
+                    db.Client.Add(user1);
+                    db.Client.Add(user2);
+                    db.SaveChanges();
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("\t" + ex.InnerException?.Message);
+            }
+        }
+    }
+}
