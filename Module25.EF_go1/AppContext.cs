665c76d254b78418996357a0449aa0887988598c
Shatalov.AG <mrstep93@ya.ru>
2024-06-29 15:56:33 +0300

    Добавьте файлы проекта.

---
+++ Module25.EF_go1/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Module25.EF_go1
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = File.ReadAllText("connString.txt");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
---
+++ Module25.EF_go1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Module25.EF_go1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Connecting to db...");
+            try
+            {
+                using (var db = new AppContext())
+                {
+                    Console.WriteLine("Connected successfuly!");
+                    var user1 = new User { Name = "Arthur", Role = "Admin" };
+                    var user2 = new User { Name = "Klim", Role = "User" };
+                    db.Users.Add(user1);
+                    db.Users.Add(user2);
+                    db.SaveChanges();
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("\t" + ex.InnerException?.Message);
+            }
+        }
+    }
+}
